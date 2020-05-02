a240b1a1172a4d37e1d1cf0a2d4c6d5b097ffa9c
ritikgarg655 <18k009@nirmauni.ac.in>
2020-05-03 00:46:04 +0530

    Succesfully implemented using STL.

--- array/STL_Implementation.cpp
+++ array/STL_Implementation.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main(){
-	
+	// syntax: array<type,size> name = {value};
+	array<int,2> arr = {1,2};
+	cout<<"Array size: "<<arr.size()<<endl;
+
+	cout<<"Printing array: ";
+	for(auto it:arr){
+		cout<<it<<" ";
+	}
+	cout<<endl;
+
+
+	// Better than this is to use vector
+	vector<int> v(5,0); // size: 5 ,value: 0
+	cout<<"Size of vector:"<<v.size()<<endl;
+	// Also indexing is allowed
+	cout<<"Value at index 0:"<<v[0]<<endl;
 }
